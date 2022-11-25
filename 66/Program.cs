1cde78a0336cfabb8acafb444029c50e3b439c49
Mila <anat500@yandex.ru>
2022-11-25 19:45:27 +0200

    9

---
+++ 66/Program.cs
@@ -0,0 +1,23 @@
+﻿void SumFromMToN(int m, int n)
+{
+    Console.Write(SumMN(m - 1, n));
+}
+int SumMN(int m, int n)
+{
+    int res = m;
+    if (m == n)
+        return 0;
+    else
+    {
+        m++;
+        res = m + SumMN(m, n);
+        return res;
+    }
+}
+Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+SumFromMToN(m, n);
+Console.WriteLine();
